846134ec8dfb443531af14c555627aa7708ae030
alumno <alumno@E125>
2019-08-15 07:56:57 -0700

    hola

---
+++ ejercicio 1/enemigo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    class enemigo
+
+    {
+        public int posx { get; set; }
+        public int posy { get; set; }
+        public int Ganar {
+            get {
+
+                return Ganar;
+            }
+            set {
+
+                if (value ==100)
+                {
+                    Ganar = value;
+                }
+                else
+                {
+                    throw new Exception("");
+                }
+            }
+        }
+
+        public void
+    }
+}
